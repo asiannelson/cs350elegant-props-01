94863ef5e95c75d14df61d9a5dc24f348ebbe8f2
Nelson <nelson14h@yahoo.com>
2018-04-17 12:13:20 -0600

    nhsiao-hw9

--- messenger/models.py
+++ messenger/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class message(models.Model):
+	subject = models.CharField(max_length=248)
+	message = models.TextField()
+	date = models.DateField(auto_now=True)
+	email = models.EmailField(max_length=254)
--- properties/models.py
+++ properties/models.py
@@ -4,11 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Property(models.Model):
-    prop_type = models.CharField(max_length=48)
-    picture_url = models.CharField(max_length=256)
-    description = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=10)
-    nearest_elementary = models.CharField(max_length=256)
-
-
-
+	prop_type = models.CharField(max_length=48)
+	picture_url = models.CharField(max_length=256)
+	description = models.TextField()
+	price = models.DecimalField(decimal_places=2, max_digits=10)
+	nearest_elementary = models.CharField(max_length=256)
+	
+	zipcode = models.IntegerField(null=True)
